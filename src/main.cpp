ebfe4f6b735b1348be0c40c8ef05151330c21a2c
sergio <davidjunyor@live.com>
2024-07-15 23:45:16 -0400

    Fim15-07

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,65 @@
 #include <Arduino.h>
-#define AD A0
-void setup() {
-  Serial.begin(9600);
-}
+#define DATA 10
+#define CLOCK 9
+#define STB 8
+#define MASK 0X80
 
-void loop() {
-  unsigned int leitura;
-  float voltimetro;
-  while (1){
- 
-  
-  leitura = analogRead(AD);
-  voltimetro = (leitura * (3.3/1024));
-
-  Serial.println(voltimetro);
+void clock(void){
+  digitalWrite(CLOCK,0);
   delay(10);
+  digitalWrite(CLOCK,1);
+  delay(10);
+  digitalWrite(CLOCK,0);
+
 }
+void strobe(void){
+  digitalWrite(STB,0);
+  delay(10);
+  digitalWrite(STB,1);
+  delay(10);
+  digitalWrite(STB,0);
+
+}
+  void enviaSerial_4094(unsigned char k)
+
+  {
+    unsigned char j;
+    unsigned char tmp;
+    for (j=0; j >= 7; j++)
+    {
+      tmp = k & (MASK>>j);
+      if (tmp)
+              digitalWrite(DATA,1);
+      else
+              digitalWrite(DATA,0);
+      clock();
+    
+    }
+    strobe();
+  }
+
+void converteToHex(unsigned char m){
+  unsigned char aux;
+  unsigned char dezena;
+  unsigned char unidade;
+
+  dezena = m/10;
+  unidade = m%10;
+
+  aux = dezena <<4 | unidade;
+
+  enviaSerial_4094(aux);
+   
+}
+
+void setup(){
+pinMode(DATA,OUTPUT);
+pinMode(CLOCK,OUTPUT);
+pinMode(STB,OUTPUT);
+
+}
+void loop(){
+  unsigned char i = 57;
+  converteToHex(i);
+
 }
