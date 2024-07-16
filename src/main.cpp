af09c190226388b9f75e43739e6ae75b1a60a6a5
sergio <davidjunyor@live.com>
2024-07-15 22:52:05 -0400

    Primeiro run

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include <Arduino.h>
 #define AD A0
 void setup() {
-
+  Serial.begin(9600);
 }
 
 void loop() {
+  unsigned int leitura;
+  float voltimetro;
+  while (1){
+ 
+  
+  leitura = analogRead(AD);
+  voltimetro = (leitura * (3.3/1024));
 
+  Serial.println(voltimetro);
+  delay(10);
 }
-
+}
